e25c68a00ee5cca91bf7c20f4b0ef54d99474342
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-05-09 10:41:05 +1200

    get-html: add toc script to html document

--- util/get-html.js
+++ util/get-html.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const marked = require('./get-marked-with-highlighter');
 
 /**
@@ -7,11 +8,14 @@ const marked = require('./get-marked-with-highlighter');
  * @param {Object} config Configuration object
  * @param {String[]} config.body_class List of classes to append to the body tag
  * @param {Object} config.marked_options Options for marked
+ * @param {number} config.toc_depth Depth for table of contents
  */
 module.exports = (md, headTags, config) => `<!DOCTYPE html>
 <html><head>
 ${headTags.join('\n')}
 </head><body class="${config.body_class.join(' ')}">
 ${marked(config)(md)}
+<script src="file://${path.resolve(__dirname, '..', 'add-toc.js')}"></script>
+<script>addToc(${config.toc_depth});</script>
 </body></html>
 `;
